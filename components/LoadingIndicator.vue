086fa606f2fc23576a6fbf7f927445f83cf2469f
Raghav Misra <raghav.m2014@gmail.com>
2020-10-25 14:24:55 -0700

    Change loader

--- components/LoadingIndicator.vue
+++ components/LoadingIndicator.vue
@@ -1,63 +1,89 @@
 <template>
-    <div class="centering-container">
-        <div class="spinner">
-            <div class="bounce1"></div>
-            <div class="bounce2"></div>
-            <div class="bounce3"></div>
+	<div class="centering-container">
+		<div class="spinner">
+            <div class="dot1"></div>
+            <div class="dot2"></div>
         </div>
-    </div>
+	</div>
 </template>
 
 <style scoped>
 .centering-container {
-    z-index: -100;
-    position: fixed;
-    top: 0; bottom: 0;
-    left: 0; right: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+	z-index: -100;
+	position: fixed;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+    transform: scale(2);
 }
 
 .spinner {
-    margin: 100px auto 0;
-    width: 70px;
-    text-align: center;
+	margin: 100px auto;
+	width: 40px;
+	height: 40px;
+	position: relative;
+	text-align: center;
+
+	-webkit-animation: sk-rotate 2s infinite linear;
+	animation: sk-rotate 2s infinite linear;
 }
 
-.spinner > div {
-    width: 18px;
-    height: 18px;
-    background-color: var(--light);
-
-    border-radius: 100%;
-    display: inline-block;
-    -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
-    animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+.dot1,
+.dot2 {
+	width: 60%;
+	height: 60%;
+	display: inline-block;
+	position: absolute;
+	top: 0;
+	background-color: var(--light);
+	border-radius: 100%;
+    box-shadow: 10px var(--primary);
+	-webkit-animation: sk-bounce 2s infinite ease-in-out;
+	animation: sk-bounce 2s infinite ease-in-out;
 }
 
-.spinner .bounce1 {
-    -webkit-animation-delay: -0.32s;
-    animation-delay: -0.32s;
+.dot2 {
+	top: auto;
+	bottom: 0;
+	-webkit-animation-delay: -1s;
+	animation-delay: -1s;
 }
 
-.spinner .bounce2 {
-    -webkit-animation-delay: -0.16s;
-    animation-delay: -0.16s;
+@-webkit-keyframes sk-rotate {
+	100% {
+		-webkit-transform: rotate(360deg);
+	}
+}
+@keyframes sk-rotate {
+	100% {
+		transform: rotate(360deg);
+		-webkit-transform: rotate(360deg);
+	}
 }
 
-@-webkit-keyframes sk-bouncedelay {
-    0%, 80%, 100% { -webkit-transform: scale(0) }
-    40% { -webkit-transform: scale(1.0) }
+@-webkit-keyframes sk-bounce {
+	0%,
+	100% {
+		-webkit-transform: scale(0);
+	}
+	50% {
+		-webkit-transform: scale(1);
+	}
 }
 
-@keyframes sk-bouncedelay {
-    0%, 80%, 100% { 
-        -webkit-transform: scale(0);
-        transform: scale(0);
-    } 40% { 
-        -webkit-transform: scale(1.0);
-        transform: scale(1.0);
-    }
+@keyframes sk-bounce {
+	0%,
+	100% {
+		transform: scale(0);
+		-webkit-transform: scale(0);
+	}
+	50% {
+		transform: scale(1);
+		-webkit-transform: scale(1);
+	}
 }
 </style>
